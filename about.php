886b74a1afc61e971fc481e1703314862028d406
sallieeky <sallieeky@gmail.com>
2022-08-05 17:28:08 +0800

    tes ubah

--- about.php
+++ about.php
@@ -1,16 +1,19 @@
-<?php 
+<?php
 session_start();
 include 'dbconnect.php';
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
-<title><?=$web['nama']?> - Tentang Kami</title>
+	<title><?= $web['nama'] ?> - Tentang Kami</title>
 
-<?php include 'layout/header.php';?>
+	<!-- INI PERUBAHAN YANG BARU KU TAMBAHIN -->
 
-<!-- //navigation -->
-<!-- breadcrumbs -->
+	<?php include 'layout/header.php'; ?>
+
+	<!-- //navigation -->
+	<!-- breadcrumbs -->
 	<div class="breadcrumbs">
 		<div class="container">
 			<ol class="breadcrumb breadcrumb1 animated wow slideInLeft" data-wow-delay=".5s">
@@ -19,22 +22,23 @@ include 'dbconnect.php';
 			</ol>
 		</div>
 	</div>
-<!-- //breadcrumbs -->
-<!-- about -->
+	<!-- //breadcrumbs -->
+	<!-- about -->
 	<div class="about">
 		<div class="container">
 			<h3 class="w3_agile_header">Tentang Kami</h3>
 			<div class="about-agileinfo w3layouts">
 				<div class="col-md-12 about-wthree-grids grid-top">
-					<h4><?=$web['nama']?> </h4>
-					<p class="top" style="text-align: justify;">Adalah Aplikasi Toko kripik dan Kue Kering Online Berbasis Web yang dibangun oleh <?=$author?>. Warung Uthie berada di Kota <?=$kota?>. Untuk sekarang Warung Uthie dijalankan oleh Ibu Sueryanti.Warung Uthie menjual berbagai cemilan ringan dan juga kering. Warung Uthie berdiri sejak tahun 2010</p>
+					<h4><?= $web['nama'] ?> </h4>
+					<p class="top" style="text-align: justify;">Adalah Aplikasi Toko kripik dan Kue Kering Online Berbasis Web yang dibangun oleh <?= $author ?>. Warung Uthie berada di Kota <?= $kota ?>. Untuk sekarang Warung Uthie dijalankan oleh Ibu Sueryanti.Warung Uthie menjual berbagai cemilan ringan dan juga kering. Warung Uthie berdiri sejak tahun 2010</p>
 				</div>
 				<div class="clearfix"> </div>
 			</div>
 		</div>
 	</div>
 	<!-- //about -->
-<?php include 'layout/footer.php';?>
- 
-</body>
+	<?php include 'layout/footer.php'; ?>
+
+	</body>
+
 </html>
